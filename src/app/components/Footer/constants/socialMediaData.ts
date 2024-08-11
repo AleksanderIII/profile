0d2793a2e49c34ffd513d50e89093ac035efb2fa
AleksanderIII <alex-llewton@mail.ru>
2024-08-11 22:59:10 +0300

    update footer social medias

--- src/app/components/Footer/constants/socialMediaData.ts
+++
@@ -1,6 +0,0 @@
-export const socialMedias = [
-  { name: 'linkedin', link: 'https://www.linkedin.com/in/alexmatyushik/' },
-  { name: 'telegram', link: 'https://t.me/Aleksander_Matyushik' },
-  { name: 'facebook', link: 'https://www.facebook.com/alex.llewton' },
-  { name: 'github', link: 'https://github.com/AleksanderIII' },
-];
--- src/app/components/Footer/partials/FooterSocialMedia.tsx
+++ src/app/components/Footer/partials/FooterSocialMedia.tsx
@@ -1,43 +1,17 @@
 'use client';
 
-import { Box, IconButton } from '@chakra-ui/react';
-import { socialMedias } from '../constants/socialMediaData';
-import {
-  FaLinkedin,
-  FaTelegramPlane,
-  FaFacebook,
-  FaGithub,
-} from 'react-icons/fa';
+import { Box } from '@chakra-ui/react';
 
-const getSocialMediaIcon = (name: string) => {
-  switch (name) {
-    case 'linkedin':
-      return <FaLinkedin />;
-    case 'telegram':
-      return <FaTelegramPlane />;
-    case 'facebook':
-      return <FaFacebook />;
-    case 'github':
-      return <FaGithub />;
-    default:
-      return null;
-  }
-};
+import SocialMediaIcon from '../../SocialMediaIcon/SocialMediaIcon';
+import { socialMedias } from '../../Sidebar/constants';
+
+const ICON_SIZE = 30;
 
 const FooterSocialMedia: React.FC = () => {
   return (
-    <Box display='flex' gap={8}>
-      {socialMedias.map((media) => (
-        <IconButton
-          key={media.name}
-          as='a'
-          href={media.link}
-          aria-label={media.name}
-          icon={getSocialMediaIcon(media.name)!}
-          variant='ghost'
-          colorScheme='teal'
-          sx={{ fontSize: '30px' }}
-        />
+    <Box display='flex' gap={4}>
+      {socialMedias(ICON_SIZE).map((media) => (
+        <SocialMediaIcon key={media.name} {...media} />
       ))}
     </Box>
   );
